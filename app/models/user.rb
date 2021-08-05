9724914eab868e3853aa42fb8b2d1e4d1402ab32
Christian Escutia <christianescutia8520@gmail.com>
2021-08-05 10:46:21 -0500

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
     validates :user_id, presence: true, uniqueness: true
     validates :password, presence: true
     validates :role, presence: true
-    validates :firstname, presence: true
-    validates :lastname, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
     validates :address, presence: true
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
     validates :phone_num, presence: true
